226d23742a8041c74ea2632e9f1c13949e9a1916
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-17 01:34:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
     
-    const urlField = document.querySelector(".field input");
-    const previewArea = document.querySelector(".preview-area");
-    const imgTag = previewArea.querySelector(".thumbnail");
-    const hiddenInput = document.querySelector(".hidden-input");
-    const button = document.querySelector(".download-btn");
+    const urlField = document.querySelector(".field input"); // URL giriş alanını seç
+    const previewArea = document.querySelector(".preview-area"); // Önizleme alanını seç
+    const imgTag = previewArea.querySelector(".thumbnail"); // Önizleme alanındaki resim etiketini seç
+    const hiddenInput = document.querySelector(".hidden-input"); // Gizli giriş alanını seç
+    const button = document.querySelector(".download-btn"); // İndirme butonunu seç
 
-    const isYouTubeUrl = (url) => {
-      const youtubeUrlRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\//i;
-      return youtubeUrlRegex.test(url);
+    const isYouTubeUrl = (url) => { // URL'nin YouTube URL'si olup olmadığını kontrol etmek için bir fonksiyon oluştur
+      const youtubeUrlRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\//i; // YouTube URL'leri için bir regex kullan
+      return youtubeUrlRegex.test(url); // URL'nin YouTube URL'si olup olmadığını döndür
     }
 
-    const isImageFileUrl = (url) => {
-      const imageFileUrlRegex = /\.(jpe?g|png|gif|bmp|webp)$/i;
-      return imageFileUrlRegex.test(url);
+    const isImageFileUrl = (url) => { // URL'nin resim dosyası URL'si olup olmadığını kontrol etmek için bir fonksiyon oluştur
+      const imageFileUrlRegex = /\.(jpe?g|png|gif|bmp|webp)$/i;  // Resim dosyaları için bir regex kullan
+      return imageFileUrlRegex.test(url); // URL'nin resim dosyası URL'si olup olmadığını döndür
     }
 
-    const getImageUrl = (url) => {
-      if (!url.startsWith("http://") && !url.startsWith("https://")) {
-        return "";
+    const getImageUrl = (url) => { // Verilen URL'den resim URL'si almak için bir fonksiyon oluştur
+      if (!url.startsWith("http://") && !url.startsWith("https://")) { // URL "http://" veya "https://" ile başlamıyorsa
+        return ""; // boş bir dize döndür
       }
       let imgUrl = "";
-      if (isYouTubeUrl(url)) {
-        const videoId = url.match(/(?:v=|youtu\.be\/)(.{11})/)[1];
-        imgUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-      } else if (isImageFileUrl(url)) {
-        imgUrl = url;
+      if (isYouTubeUrl(url)) { // Eğer URL YouTube URL'si ise
+        const videoId = url.match(/(?:v=|youtu\.be\/)(.{11})/)[1]; // YouTube video kimliğini al
+        imgUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`; // YouTube önizleme resmi için URL oluştur
+      } else if (isImageFileUrl(url)) { // Eğer URL resim dosyası URL'si ise
+        imgUrl = url; // URL'yi resim URL'si olarak ayarla
       }
-      return imgUrl;
+      return imgUrl; // Resim URL'sini döndür
     }
 
-    urlField.addEventListener("input", () => {
-      const imgUrl = urlField.value.trim();
-      if (imgUrl) {
-        const imageUrl = getImageUrl(imgUrl);
-        if (imageUrl) {
-          imgTag.src = imageUrl;
-          previewArea.classList.add("active");
+    urlField.addEventListener("input", () => { // URL giriş alanındaki değişiklikleri dinle
+      const imgUrl = urlField.value.trim(); // Giriş alanındaki URL'yi al ve baştaki ve sondaki boşlukları temizle
+      if (imgUrl) { // Eğer URL mevcutsa
+        const imageUrl = getImageUrl(imgUrl); // URL'den resim URL'sini al
+        if (imageUrl) { // Eğer resim URL'si mevcutsa
+          imgTag.src = imageUrl; // Resim etiketinin "src" özelliğini resim URL'siyle güncelle
+          previewArea.classList.add("active"); // Önizleme alanına "active" sınıfını ekle
           button.style.pointerEvents = "auto";
           hiddenInput.value = imageUrl;
         } else {
